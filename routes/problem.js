1d61c362017dcba49e38668994f594213c9a7d55
Packet <199sd@naver.com>
2018-11-17 21:36:37 +0900

    구조완성

---
+++ routes/problem.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/list',(req,res)=>{
+
+})
+router.get('/:num',(req,res)=>{
+
+})
+router.post('/submit/:num',(req,res)=>{
+
+})
+
